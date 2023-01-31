ad141ed88ea58a80181717fc7f74b653598483f1
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2023-01-31 14:24:35 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа_2
+{
+    //  лямбда-выражение 
+    // Метод возвращает подмножество 
+    // элементов массива случайных чисел,
+    // которые являются четными
+    // Один параметр: 
+    //размер исходного массива
+    class Program
+    {
+       // public delegate int TasksAWhileDelegate(int data, int ms);
+        static void Main(string[] args)
+        {
+            
+            Func<int, List<int>> lambda = n =>
+            {
+                Random rnd = new Random();
+                int[] ch = new int[n];
+                List<int> res = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    ch[i] = rnd.Next(1, 1000);
+                    Console.Write(ch[i] + "  ");
+                    if (ch[i] % 2 == 0)
+                        res.Add(ch[i]);
+                }
+                Thread.Sleep(3000);
+                return res;
+            };
+            Console.WriteLine("Введите количество элементов массива >");
+            int N = Convert.ToInt32(Console.ReadLine());
+            IAsyncResult ar = lambda.BeginInvoke(N, null, null);
+            while (!ar.IsCompleted)
+            {
+                Console.WriteLine("\n Загрузка!!! Подождите!!!");
+                Thread.Sleep(1000);
+            }
+            List<int> list = lambda.EndInvoke(ar);
+            Console.WriteLine("\n");
+            if (list.Count != 0)
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Write(list[i] + "  ");
+                }
+            else Console.Write("Чётных элементов нет");
+            Console.ReadLine();
+        }
+    }
+}
