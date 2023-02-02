dbd51e629dbabffcd052b2aa7813642a278c216e
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2023-02-02 13:04:03 +0300

    Вариант 19 Лабораторная работа 4

--- Лабораторная работа 4/Program.cs
+++ Лабораторная работа 4/Program.cs
@@ -9,53 +9,68 @@ using System.Threading.Tasks;
 namespace Лабораторная_работа_4
 {
 
-    // лямбдавыражение
-    // Метод возвращает 
-    // логическое значение, указывающее существует ли заданный символ в строке
-    // Два параметра: строка и искомый символ
+    // пользовательский
+    //  Метод возвращает 
+    //  количество нечетных элементов в матрице случайных чисел
+    // Два параметра: размер матрицы
     // делегат
+
+
 
     class Program
     {
-        public delegate bool TakesAWhileDelegate (string stroka, char simbol);
+        public delegate int TakesAWhileDelegate (int a, int b);
         static void Main(string[] args)
         {
-           TakesAWhileDelegate dl = (st, ch) =>
-            {
-                Thread.Sleep(500);
-                return st.Contains(ch);
-            };
-            Console.WriteLine("Введите строку >");
-            string nstr = Console.ReadLine();
-            Console.WriteLine("Введите символ >");
-            char nsim = Convert.ToChar(Console.ReadLine());
-            dl.BeginInvoke(nstr, nsim, TaskIsCompleted, dl);
+            TakesAWhileDelegate dl = Task;
+            Console.WriteLine("Введите размер матрицы >");
+            int  ma= Convert.ToInt32(Console.ReadLine());
+            int mb = Convert.ToInt32(Console.ReadLine());
+            dl.BeginInvoke(ma, mb, TaskIsCompleted, dl);
             for (int i = 0; i < 100; i++)
             {
                 Console.Write(".");
                 Thread.Sleep(50);
             }
+            Console.ReadLine();
 
-                //dl.BeginInvoke(1, 3000, ar=>
-                //{
-                //    int result = dl.EndInvoke(ar);
-                //    Console.WriteLine("result {0}", result);
-                //}, null);
-                //for (int i=0; i<100; i++)
-                //{
-                //    Console.Write(".");
-                //    Thread.Sleep(50);
-                //}
+            //dl.BeginInvoke(1, 3000, ar=>
+            //{
+            //    int result = dl.EndInvoke(ar);
+            //    Console.WriteLine("result {0}", result);
+            //}, null);
+            //for (int i=0; i<100; i++)
+            //{
+            //    Console.Write(".");
+            //    Thread.Sleep(50);
+            //}
         }
 
+        static int Task (int a, int b)
+        {
+            int[,] matrix = new int[a, b];
+            int k = 0;
+            Random rnd = new Random();
+            for(int i=0; i<a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    matrix[i, j] = rnd.Next(100);
+                    Console.Write(matrix[i, j] + "   ");
+                    if (matrix[i, j] % 2 != 0) k += 1;
+                }
+                Console.Write("\n");
+            }
+            return k;
+        }
         static void TaskIsCompleted (IAsyncResult ar)
         {
             if (ar == null) throw new ArgumentNullException("ar");
                 TakesAWhileDelegate dl = ar.AsyncState as TakesAWhileDelegate;
                 Trace.Assert(dl != null, "Invalid object type");
-                bool result = dl.EndInvoke(ar);
+                int result = dl.EndInvoke(ar);
                 Console.WriteLine("result {0}", result);
-                Console.ReadLine();
+            
         }
 
         //static void TakesAWhileCompleted (IAsyncResult ar)
